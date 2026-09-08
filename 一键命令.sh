03a9adaba4f27cefb144a4a343835a935f1e8668
programGod <foxorg@outlook.com>
2026-09-08 23:49:16 +0800

    Update and rename 编码后.sh to 一键命令.sh

---
+++ 一键命令.sh
@@ -0,0 +1,5 @@
+部署安装
+bash <(curl -sL https://raw.githubusercontent.com/scottorg/Cobaltstrike4.7CN-Auto-Install/main/install.sh)
+
+卸载
+bash <(curl -sL https://raw.githubusercontent.com/scottorg/Cobaltstrike4.7CN-Auto-Install/main/cleanup.sh)
